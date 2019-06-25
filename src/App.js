f6af71737f0d10460ba936e937f084ac326f0bf2
sawa <sw41dc@gmail.com>
2019-06-25 23:41:18 +0900

    :recycle: Use onChange event

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ const App = () => (<Counter></Counter>)
 class Counter extends Component {
   constructor(props) {
     super(props)
-    this.state = { count: 1 }
+    this.state = {
+      count: 1,
+      text: "sample text"
+    }
   }
 
   handlePlusButton = () => {
@@ -20,12 +23,23 @@ class Counter extends Component {
     })
   }
 
+  updateText = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <div>count: { this.state.count }</div>
         <button onClick={this.handlePlusButton}>+1</button>
         <button onClick={this.handleMinusButton}>-1</button>
+        <br />
+        <br />
+        <input type="text" onChange={this.updateText} />
+        <br />
+        {this.state.text}
       </React.Fragment>
     )
   }
