a825b32d556632070a9a082eac5f3cc33b424bc6
sawa <sw41dc@gmail.com>
2019-07-01 22:07:31 +0900

    :sparkles: Add function of UPDATE and use Material-UI

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
-import { getEvent, deleteEvent } from '../actions'
+import { getEvent, deleteEvent, putEvent } from '../actions'
 
 class EventsShow extends Component {
   constructor(props) {
@@ -21,10 +23,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField 
+        hintText={label}
+        floatingLabelText={label}
+        rype={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -35,22 +41,24 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
- //   await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
+
         <div><Field lable="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field lable="Body" name="body" type="text" component={this.renderField} /></div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
-          <Link to="/">Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
+        <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
         </div>
       </form>
     )
@@ -71,7 +79,7 @@ const mapStateToProps = (state, ownProps) => {
   return { initialValues: event, event }
 }
 
-const mapDispatchToProps = ({ deleteEvent, getEvent })
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true})(EventsShow)
